4874fc22f9a11d0c4ec639e0dadd846b45daec29
Lovlin Thakkar <lovlin@Lovlins-MacBook-Air.local>
2020-06-01 01:10:06 +0530

    add experience as config

--- javascript/main.js
+++ javascript/main.js
@@ -17,6 +17,12 @@ function openTab(evt, tabName) {
 }
 
 
+function onLoad() {
+  populateSkills();
+  populateExperience();
+}
+
+
 function populateSkills() {
   console.log("Populating skills!");
 
@@ -26,10 +32,37 @@ function populateSkills() {
 
       var button = document.createElement("button");
       button.innerHTML = skill;
-      button.className="button";
-      button.style.background="hsl(0, 0%, 92%)";
-      button.style.border="none";
+      button.className = "button";
+      button.style.background = "hsl(0, 0%, 92%)";
+      button.style.border = "none";
       document.getElementById("skillsDiv").appendChild(button);
+    });
+  });
+}
+
+function populateExperience() {
+  console.log("Populating experience!");
+
+  $.getJSON('javascript/config.json', function(data) {
+    $.each(data.experiences, function(i, experience) {
+      console.log(`Experience #${i}: ${experience}`);
+
+      var division = document.createElement("div");
+      division.className="notification is-white-ter";
+
+      var title = document.createElement("b");
+      title.innerHTML = experience.title + " @ " + experience.company + " (" + experience.timeline + ")";
+      division.appendChild(title);
+
+      var descriptionList = document.createElement("ul");
+      for (let j = 0; j < experience.description.length; j++) {
+        var bulletPoint = document.createElement("li");
+        bulletPoint.innerHTML = experience.descriptionList[j];
+        descriptionList.appendChild(bulletPoint);
+      }
+      division.appendChild(descriptionList);
+
+      document.getElementById("2").appendChild(division);
     });
   });
 }
